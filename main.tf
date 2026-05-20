0922c82937c61f91bebb04e5fab9aa7fd1092f33
Raphael Pothin <raphael.pothin@gmail.com>
2026-05-19 20:39:05 -0400

    Remove test comment from main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# ─── Data sources: security role and root business unit lookup ──────────────── Test
+# ─── Data sources: security role and root business unit lookup ────────────────
 
 data "powerplatform_data_records" "root_business_unit" {
   environment_id    = var.host_environment_id
